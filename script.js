6c12f16d0e078a2622a500b3f266f88f54f0ea72
nexusghost1225 <JGBxmas2000@hotmail.com>
2022-11-11 00:02:56 -0500

    added final variables

--- script.js
+++ script.js
@@ -21,54 +21,69 @@ function generatePassword() {
     }
 
     //ask for character types
-var promptNumbers = window.confirm("Would you like your passcode to include numbers?");
-var promptLowercase = window.confirm("would you like your passcode to contain lowrcase numbers?");
-var promptUppercase = window.confirm("would you like your asscode to contain uppercase letters?");
-var promptSpecial = window.confirm("would you like your passcode to contain special characters?");
+    var promptNumbers = window.confirm("Would you like your passcode to include numbers?");
+    var promptLowercase = window.confirm("would you like your passcode to contain lowrcase numbers?");
+    var promptUppercase = window.confirm("would you like your asscode to contain uppercase letters?");
+    var promptSpecial = window.confirm("would you like your passcode to contain special characters?");
 
 //arrays of each characters
-cosnt arLowercase = ["abcdefghijklmnopqrstuvwxyz"];
-conts arUppercase = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"];
-conts arNumber = ["0123456789"];
-conts arSpecial = ["!@#$%^&*-_`~"];
+const arLowercase = ["abcdefghijklmnopqrstuvwxyz"];
+const arUppercase = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"];
+const arNumber = ["0123456789"];
+const arSpecial = ["!@#$%^&*-_`~"];
 
-//define master list of characters
-var masterList = [];
+    //define master list of characters
+    var masterList = [];
 
-//define a list if chosen types
-var chosen = [];
+    //define a list if chosen types
+    var chosen = [];
 
-//if statements
-if (promptNumbers === true) {
-    for (var i=o; i <13; i++ ){
-        var masterList = arNumbers.concat(masterList)
+    //if statements
+    if (promptNumbers === true) {
+        for (var i = o; i < 13; i++) {
+            var masterList = arNumbers.concat(masterList)
+        }
     }
-}
-if (promptLowercase === true) {
-    for (var i=0; i < 5; i++) {
-        var masterList = arLowercase.concat(masterList);
+    if (promptLowercase === true) {
+        for (var i = 0; i < 5; i++) {
+            var masterList = arLowercase.concat(masterList);
+        }
+        chosen.push("lowercase letters")
     }
-chosen.push("lowercase letters")
-}
-if (promptUppercase === true) {
-    for (var i = 0; i < 5; i++) {
-        var masterList = arUppercase.concat(masterList);
-    } 
-    chosen.push("uppercase letters")
-}
-if (promptSpecial=== true) {
- for (var i = 0; i = < 12; i++) {
-    var masterList = arSpecial.concat(masterList);
- }   
- chosen.push("special characters")
-}
-//if no choices weree selected
-if (!promptSpecial &&
-    !promptUppercase &&
-    !promptNumbers &&
-    !promptLowercase) {
+    if (promptUppercase === true) {
+        for (var i = 0; i < 5; i++) {
+            var masterList = arUppercase.concat(masterList);
+        }
+        chosen.push("uppercase letters")
+    }
+    if (promptSpecial === true) {
+        for (var i = 0; i < 12; i++) {
+            var masterList = arSpecial.concat(masterList);
+        }
+        chosen.push("special characters")
+    }
+
+    //if no choices weree selected
+    if (!promptSpecial &&
+        !promptUppercase &&
+        !promptNumbers &&
+        !promptLowercase) {
         window.alert("You must select atleast one character type")
         var masterList = arLowercase.concat(masterList);
         chosen.push("lowercase letters")
     }
-    
+
+    //user alert windows
+    chosen = chosen.join()
+    window.alert("your password will be" = promptLength + "characters long and contain" + chosen + ".")
+
+    // shuffles main character list
+    masterList.sort(() => Math.random() - 0.5)
+
+    // sets length of passcode
+    var postList = masterList.slice(0, promptLength);
+    // combines array into single string
+    var finalPass = (postList.join(''));
+    // sends final passcode out
+    return (finalPass);
+}
