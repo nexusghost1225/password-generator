4b1350954734ef7b220bf7a04bf81a4a3e4ac608
nexusghost1225 <JGBxmas2000@hotmail.com>
2022-11-10 20:09:07 -0500

    finished html

---
+++ script.js
@@ -0,0 +1,6 @@
+//assignment code
+var generateBtn = document.querySelector("generate")
+//password generator 
+function generatePassword(){
+//ask for passwordk length
+var promptLength = window.prompt("How many characters do you require for the password (8-120)")
